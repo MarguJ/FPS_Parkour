6b6770378843f5290048ac12610765ede2adc872
MarguJ <margulesjoa@gmail.com>
2025-05-16 09:08:18 -0300

    Parkour super dificil imposible

---
+++ Assets/Scripts/RestartSceneOnFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deathFallHeight = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Respawned");
+        if (transform.position.y < deathFallHeight)
+        {
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
